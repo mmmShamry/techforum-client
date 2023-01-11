28554c9ed176dfa9e2b41e24447ceab5038b0d66
MohamedShamry <mmmshamry@gmail.com>
2023-01-12 00:05:54 +0530

    ask question page

--- src/views/Questions/AskQuestion/index.js
+++ src/views/Questions/AskQuestion/index.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../../components/Layout";
 import Select from "react-select";
 import axios from "axios";
+import TextField from "@mui/material/TextField";
+import { Box } from "@mui/material";
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 const AskQuestion = () => {
   const [tags, setTags] = useState([]);
   const [tagOpetions, setTagOptions] = useState([]);
   const [selectedOpetions, setSelectedOptions] = useState([]);
+  const [title, setTitle] = useState('');
+
+  const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
 
   const getTags = async () => {
     const response = await axios.get("tag_controller");
@@ -31,14 +40,33 @@ const AskQuestion = () => {
 
   return (
     <Layout>
-      <Select
-        isMulti
-        name="tags"
-        options={tagOpetions}
-        placeholder="Tags"
-        value={selectedOpetions}
-        onChange={handleSelect}
-      />
+      <Box>
+        <Box mb={2}>
+          <TextField
+            label="Title"
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+          />
+        </Box>
+
+        <Box mb={2}>
+          <Select
+            isMulti
+            name="tags"
+            options={tagOpetions}
+            placeholder="Tags"
+            value={selectedOpetions}
+            onChange={handleSelect}
+          />
+        </Box>
+
+        <Box mb={2}>
+          <Editor editorState={editorState} onChange={setEditorState} />;
+        </Box>
+      </Box>
     </Layout>
   );
 };
